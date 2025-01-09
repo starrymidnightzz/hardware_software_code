12644b73a039abac3f7728eb0fd1a2c54d9efbc4
Anna <moonstarfest@gmail.com>
2025-01-09 05:44:43 -0500

    my first commit

---
+++ lab02.py
@@ -0,0 +1,11 @@
+def main():
+    name = "Professor Nedd"
+    print("We want to know if you like programming!")
+    print()
+    print("Do you like programming {}?".format(name))
+    answer = input()
+    print("Great! You said {}?".format(answer))
+    print("Let’s start learning some Python today")
+    
+if __name__ == "__main__":
+    main()

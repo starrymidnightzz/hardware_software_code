f253223ec921b8861cc5d47ffd3fb32723263189
Anna <moonstarfest@gmail.com>
2025-01-14 13:32:15 -0500

    completed 1/14/25

---
+++ AL_project_01.py
@@ -0,0 +1,37 @@
+# Python is a computer programming language that lets you work swiftly and integrate systems more efficiently.
+
+# <Saving this coding example (perhaps for something in the future?)>
+# "So your name is {}".format(name), "and you currently go to {}".format(school)?
+
+
+def main():
+    print("Hello and welcome! Python is a computer programming language that lets you work swiftly and integrate systems more efficiently.")
+    print("We want to know if you like programming!")
+    print("Do you like programming?")
+    answer = input()
+
+    print()
+    print("Great! You said {}?".format(answer))
+    print("Let’s start learning some Python today. But before that, I would like for you to answer a few questions first.")
+    blank = input()
+
+    print()
+    print("What's your name?")
+    name = input()
+
+    print()
+    print("What high school did you attend?")
+    school = input()
+
+    print()
+    print("What college do you currently attend?")
+    college = input()
+
+    print()
+    print("It's great to get to know you more! I've learned that your name is {},".format(name.capitalize()), "you attended {}".format(school), "for high school, and you're currently attending {}.".format(college))
+    print("It was fun getting to know you.")
+    print("Now let's get started!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ lab06.py
@@ -0,0 +1,10 @@
+def greeting(name):
+    print("Hi {} ".format(name.capitalize()))
+    print("Welcome to my conversation program!!!")
+    print("Glad to have you in Hardware & Software!!!")
+
+def main():
+    greeting(input("What is your name?"))
+
+if __name__ == "__main__":
+    main()

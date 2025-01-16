41927eb26f3653af22d12b2e5ce9c41375650aa7
Anna <moonstarfest@gmail.com>
2025-01-16 13:24:23 -0500

    completed 1/16/25

---
+++ lab07.py
@@ -0,0 +1,18 @@
+def conversation():
+    print("Do you like coding in python? Answer yes or no")
+    answer = input().lower().strip()
+    if answer == "yes":
+        print("That's good - the United States needs more coders!!")
+    elif answer == "no":
+        print("Perhaps you will change your mind ")
+    else:
+        print("I don't understand?")
+        conversation()
+
+def main():
+    print("Welcome to my conversation program")
+    conversation()
+    print("Thanks for chatting with me")
+    
+if __name__ == "__main__":
+    main()

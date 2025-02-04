c819f2988075cb79a1a4070dbd594e7b6f6f66c6
Anna <moonstarfest@gmail.com>
2025-02-04 12:52:46 -0500

    completed 1/27/25, updated 2/4/25

--- lab09.py
+++ lab09.py
@@ -4,10 +4,10 @@ def print_lyrics():
 
 def repeat_lyrics(count = 1):
     for number in range(count):
-        print("{}############".format(number), end=".")
+        print("{}".format(number))
         print_lyrics()
-    print("############")
-
+    print("Done Repeating!!")
+    print("################")
 
 def main():
     repeat_lyrics()

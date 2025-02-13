a1a8456bd72f442508a2f5730f24a703154082db
Anna <moonstarfest@gmail.com>
2025-02-13 13:13:32 -0500

    completed 2/13/25

--- lab10.py
+++ lab10.py
@@ -18,17 +18,3 @@ def loop():
 if __name__ == "__main__":
     main()
     loop()
-
-    # figure out how to loop code
-
-# def while_loop(num):
-    # while num > 0:
-        # print("While count:{}".format(num))
-        # num += -1
-# def for_loop(num):
-    # for count in range(num,0, -1):
-        # print("For count:{}".format(count))
-# def main():
-    # counter = 10
-    # while_loop(counter)
-    # for_loop(counter)

1b56262cd5f9be270ed1c9cb554ef7fbb4b8b684
Anna <moonstarfest@gmail.com>
2025-02-19 13:08:16 -0500

    completed 2/19/25

--- AL_project_02.py
+++ AL_project_02.py
@@ -1,20 +1,27 @@
-def main():
+def conversation():
     print("Welcome to my conversation program")
     print("I will keep asking questions until you type 'exit'.")
 
-    print("What is your name?")
-    name = input()
+def questions():
+    name = input("What is your name?")
+    color = input("What's your favorite color, {}?".format(name))
+    answer = input("Do you like video games, {}?".format(name))
+    show = input("What's your favorite TV show, {}?".format(name))
+    sport = input("What's your favorite sport, {}?".format(name))
+    food = input("What's your favorite food, {}?".format(name))
+    preference = input("Do you prefer coffee or tea, {}?".format(name))
 
-    print("What's your favorite color, {}?".format(name))
-    color = input()
+    return input("Type exit to leave program."), name
 
-    print("Do you like video games, {}?".format(name))
-    answer = input()
+def main():
+    count = 0
+    run_loop, name = questions()
 
-    if input() != 'exit':
-        print("Thanks for chatting with me, {}".format(name))
-        print("You answered questions.")
+    while run_loop != 'exit':
+        run_loop, name = questions()
+
+    print("Thanks for chatting with me, {}".format(name))
+    print("You answered", str(count), "questions.")
 
 if __name__ == "__main__":
     main()
- 
